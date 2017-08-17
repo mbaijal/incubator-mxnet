292ba29663bb2b11c1d08b84cfe5de6a8c0a5f19
mbaijal <30911248+mbaijal@users.noreply.github.com>
2017-08-16 17:58:33 -0700

    Test 2: Edit the Jenkinsfile to call run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -6,16 +6,16 @@ HASH=$(git rev-parse HEAD)
 TAG=$(basename $(git describe --all --exact-match ${HASH}))
 
 echo "HASH: $HASH"
-echo "RELEASE_TAG: $RELEASE_TAG"
+echo "TAG: $TAG"
 
-DEVICES=('cpu' 'gpu')
-LANGUAGES=('python' 'julia' 'r-lang' 'scala' 'perl')
+DEVICES=('cpu')
+LANGUAGES=('python')
 for DEV in "${DEVICES[@]}"; do
     for LANG in "${LANGUAGES[@]}"; do
         ./tool.sh build ${LANG} ${DEV} ${HASH}
         ./tool.sh test ${LANG} ${DEV} ${HASH}
-        if [[ -n "$RELEASE_TAG" && "$IS_CI" == true ]]; then
-            # Push if triggered by tagging event and running in CI environment
+        if [[ -n "$TAG" && "$IS_PUBLISH" == true ]]; then
+            # Push if triggered by tagging event and IS_PUBLISH is set
             ./tool.sh push ${LANG} ${DEV} ${HASH}
         fi
     done
