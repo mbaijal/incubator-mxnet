f03462f5ea7b1b530825137145c3111945f16865
mbaijal <30911248+mbaijal@users.noreply.github.com>
2017-08-30 14:00:36 -0700

    creating a new branch with rc3 and docker release scripts

--- docker/run.sh
+++ docker/run.sh
@@ -19,11 +19,27 @@
 
 # Build and push all docker containers
 
+HASH=$(git rev-parse HEAD)
+TAG=$(basename $(git describe --all --exact-match ${HASH}))
+RELEASE_TAG=$(basename $(git tag -l --contains ${HASH}))
+IS_PUBLISH=false
+
+echo "HASH: $HASH"
+echo "TAG: $TAG"
+echo "IS_PUBLISH: $IS_PUBLISH"
+echo "RELEASE_TAG: $RELEASE_TAG"
+
+
 DEVICES=('cpu' 'gpu')
-LANGUAGES=('python' 'julia' 'r-lang' 'scala' 'perl')
+LANGUAGES=('python' 'julia' 'scala' 'perl')
 for DEV in "${DEVICES[@]}"; do
     for LANG in "${LANGUAGES[@]}"; do
-        ./tool.sh build ${LANG} ${DEV}
-        ./tool.sh push ${LANG} ${DEV}
+        ./tool.sh build ${LANG} ${DEV} ${HASH}
+        ./tool.sh test ${LANG} ${DEV} ${HASH}
+        if [[ -n "$TAG" && "$IS_PUBLISH" == true ]]; then
+            # Push if triggered by tagging event and IS_PUBLISH is set
+            echo "Alert! Pushing to DockerHub, using account mbaijal!"
+            ./tool.sh push ${LANG} ${DEV} ${HASH}
+        fi
     done
 done
