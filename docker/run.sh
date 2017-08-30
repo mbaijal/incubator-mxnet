c986b7996fb33b02cf46e9f142b5991c098d0a08
mbaijal <30911248+mbaijal@users.noreply.github.com>
2017-08-30 12:31:49 -0700

    editing scripts for manual docker release

--- docker/run.sh
+++ docker/run.sh
@@ -5,6 +5,8 @@
 HASH=$(git rev-parse HEAD)
 TAG=$(basename $(git describe --all --exact-match ${HASH}))
 RELEASE_TAG=$(basename $(git tag -l --contains ${HASH}))
+#only for release, meghna, update back to variable
+IS_PUBLISH=false
 
 echo "HASH: $HASH"
 echo "TAG: $TAG"
@@ -12,8 +14,8 @@ echo "IS_PUBLISH: $IS_PUBLISH"
 echo "RELEASE_TAG: $RELEASE_TAG"
 
 
-DEVICES=('cpu')
-LANGUAGES=('python')
+DEVICES=('cpu' 'gpu')
+LANGUAGES=('python' 'julia' 'scala' 'perl')
 for DEV in "${DEVICES[@]}"; do
     for LANG in "${LANGUAGES[@]}"; do
         ./tool.sh build ${LANG} ${DEV} ${HASH}
