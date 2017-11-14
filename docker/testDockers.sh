89eac1192d415e887fd8ed0c57c43153dc633c36
mbaijal <30911248+mbaijal@users.noreply.github.com>
2017-11-14 13:34:55 -0800

    DockerPipelineWIP_3

--- docker/testDockers.sh
+++ docker/testDockers.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+
+#Add tests to check that the image is the one just created - Check the version or the tag
     set -e
+
+    #1. Python Test
     if [[ "${LANGUAGE}" == "python" ]]; then
         if [[ "${DEVICE}" == "gpu" ]]; then
             ${DOCKER_BINARY} run --rm -w /mxnet mxnet/python:gpu bash -c "python tests/python/train/test_conv.py --gpu"
@@ -9,6 +13,7 @@
         exit
     fi
 
+    #2. r-lang Test
     if [[ "${LANGUAGE}" == "r-lang" ]]; then
         if [[ "${DEVICE}" == *"gpu"* ]]; then
             ${DOCKER_BINARY} run --rm -w /mxnet mxnet/r-lang:gpu bash -c "cd R-package/demo/; Rscript basic_model.R --gpu"
@@ -18,6 +23,7 @@
         exit
     fi
 
+    #3. Scala Test
     if [[ "${LANGUAGE}" == "scala" ]]; then
         if [[ "${DEVICE}" == *"gpu"* ]]; then
             ${DOCKER_BINARY} run --rm -w /mxnet mxnet/scala bash -c "cd scala-package/core/; scripts/get_mnist_data.sh; cd /mxnet/; sh scala-package/examples/scripts/module/run_sequential_module.sh --gpu"
@@ -27,6 +33,7 @@
         exit
     fi
 
+    #4. Julia Test
     if [[ "${LANGUAGE}" == "julia" ]]; then
         if [[ "${DEVICE}" == *"gpu"* ]]; then
             # TODO: change this test so we can pass in "gpu" parameter to run on gpu instead
@@ -37,7 +44,7 @@
         exit
     fi
 
-
+    #5. Perl Test
     if [[ "${LANGUAGE}" == "perl" ]]; then
         if [[ "${DEVICE}" == *"gpu"* ]]; then
             ${DOCKER_BINARY} run -w /mxnet --rm mxnet/julia:gpu bash -c "perl perl-package/AI-MXNet/examples/mnist.pl --gpus 0"
