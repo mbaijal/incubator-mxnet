07570e332c4f857d4a5a6413bd3a76f990a3f746
mbaijal <30911248+mbaijal@users.noreply.github.com>
2018-04-20 17:59:37 -0700

    add more paths for rat check

--- tests/nightly/apache_rat_license_check/license_check.sh
+++ tests/nightly/apache_rat_license_check/license_check.sh
@@ -37,8 +37,10 @@ echo "build success, cd into target"
 cd apache-rat/target
 
 sudo chmod -R 777 /home/ubuntu/workspace/NightlyPipeline_onSource/trunk
+sudo chmod -R 777 /work/mxnet/trunk/apache-rat-tasks
 sudo rm -y /home/ubuntu/workspace/NightlyPipeline_onSource/trunk/apache-rat-tasks/src/test/resources/antunit/report-normal-operation.xml
 sudo rm -y /home/ubuntu/workspace/NightlyPipeline_onSource/trunk/apache-rat-tasks/src/test/resources/antunit/report-bad-configurations.xml
+sudo rm -y /work/mxnet/trunk/apache-rat-tasks/src/test/resources/antunit/report-normal-operation.xml
 
 echo "run apache RAT check"
 java -jar apache-rat-0.13-SNAPSHOT.jar -E /home/ubuntu/workspace/NightlyPipeline_onSource/tests/nightly/apache_rat_license_check/.rat-excludes -d /home/ubuntu/workspace/NightlyPipeline_onSource
