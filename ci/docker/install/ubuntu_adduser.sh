a93fcf9eb6186b6151f527e0c226a19d4e9824e5
mbaijal <30911248+mbaijal@users.noreply.github.com>
2018-04-30 16:53:22 -0700

    run on official ci

--- ci/docker/install/ubuntu_adduser.sh
+++ ci/docker/install/ubuntu_adduser.sh
@@ -29,7 +29,6 @@ then
     # -no-log-init required due to https://github.com/moby/moby/issues/5419
     useradd -m --no-log-init --uid $USER_ID --system jenkins_slave 
     usermod -aG sudo jenkins_slave
-    usermod -aG sudo 1000
 
     # By default, docker creates all WORK_DIRs with root owner    
     mkdir /work/mxnet    
