e3f099bcd5634e74cddf676269959e864b4d5659
mbaijal <30911248+mbaijal@users.noreply.github.com>
2018-05-02 20:33:55 -0700

    try again

--- ci/docker/runtime_functions.sh
+++ ci/docker/runtime_functions.sh
@@ -562,8 +562,8 @@ nightly_test_compilation_warning() {
 
 nightly_test_installation() {
     set -ex
-    chmod -R 777 ./tests/jenkins/run_test_installation_docs.sh
-    tests/jenkins/run_as_user.sh `id -u` `id -un` `id -g` `id -un` source ./tests/jenkins/run_test_installation_docs.sh docs/install/index.md 1 1686; ${1}
+    chmod -R 777 /tests/jenkins
+    source ./tests/jenkins/run_test_installation_docs.sh docs/install/index.md 1 1686; ${1}
 }
 
 nightly_test_image_classification() {
